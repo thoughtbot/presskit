f9e97de2ad0709f14d67217c012743d42d8421f2
Tyson Gach <tyson@tysongach.com>
2018-06-08 10:54:08 -0400

    Use CSS for "sticky" nav interaction

--- source/assets/javascripts/sticky-nav.js
+++
@@ -1,9 +0,0 @@
-var elementPosition = $(".nav-wrap").offset();
-
-$(window).scroll(function(){
-  if($(window).scrollTop() > elementPosition.top) {
-    $(".nav-wrap").css("position", "fixed").css("top", "0");
-  } else {
-    $(".nav-wrap").css("position", "static");
-  }
-});
