48b6eb35f6e53a12de177faa808b7a219ed12dc6
Tyson Gach <tyson@tysongach.com>
2018-11-17 09:49:27 -0500

    Remove middleman-livereload

--- config.rb
+++ config.rb
@@ -5,12 +5,6 @@ set :fonts_dir, "assets/fonts"
 set :images_dir, "assets/images"
 set :js_dir, "assets/javascripts"
 
-configure :development do
-  activate :livereload do |reload|
-    reload.no_swf = true
-  end
-end
-
 configure :production do
   activate :gzip
   activate :minify_css
