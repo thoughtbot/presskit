1c49e8edea4dcd9e0f5fab9054bed30e1f094b2d
Tyson Gach <tyson@tysongach.com>
2019-07-09 11:35:44 -0400

    Enable asset hashing

--- config.rb
+++ config.rb
@@ -5,6 +5,7 @@ config[:images_dir] = "assets/images"
 config[:sass_assets_paths] = ["node_modules"]
 
 configure :production do
+  activate :asset_hash
   activate :gzip
   activate :minify_css
   activate :minify_html
