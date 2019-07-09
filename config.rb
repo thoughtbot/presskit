d20518899b4fe9ac8411bdac1d5892169ed88ac9
Tyson Gach <tyson@tysongach.com>
2019-07-09 14:39:27 -0400

    Set config options

--- config.rb
+++ config.rb
@@ -1,7 +1,9 @@
 activate :autoprefixer
 
 config[:css_dir] = "assets/stylesheets"
+config[:fonts_dir] = "assets/fonts"
 config[:images_dir] = "assets/images"
+config[:js_dir] = "assets/javascripts"
 config[:sass_assets_paths] = ["node_modules"]
 
 configure :production do
